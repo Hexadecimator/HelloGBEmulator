e1e6d4b7c3f2264efd1512d2d752c910cc382efb
Logan Scholz <logans@ping.com>
2025-09-02 13:41:44 -0700

    fleshing out opcodes a little more, improved cpu.cpp and cpu.h

--- cpuLR35902.cpp
+++ cpuLR35902.cpp
@@ -4,12 +4,70 @@
 cpuLR35902::cpuLR35902()
 {
 	using a = cpuLR35902;
+	// each {} is an INSTRUCTION struct: 
+	// string name, pointer to opcode function, total cycles for instruction
+	// TODO 1: done
+	// TODO 2: There are a lot of instructions that operate on different registers (LD for example), 
+	//         will one LD function satisfy all the permutations? How to do this?
+	// TODO 3: Every opcode also has "length in bytes" that is not captured here... should it be? 
+	//         ...maybe this "length in bytes" attribute can be used to organize some kind of polymorphism?
 	lookup =
 	{
-		{}
+		// only doing 1 row for now to figure things out
+		{"NOP", &a::NOP, 4},{"LD", &a::LD, 12},{"LD", &a::LD, 8},{"INC", &a::INC, 8},{"INC", &a::INC, 4},{"DEC", &a::DEC, 4},{"LD", &a::LD, 8},{"RLCA", &a::RLCA, 4},{"LD", &a::LD, 20},{"ADD", &a::ADD, 8},{"LD", &a::LD, 8},{"DEC", &a::DEC, 8},{"INC", &a::INC, 4},{"DEC", &a::DEC, 4},{"LD", &a::LD, 8},{"RRCA", &a::RRCA, 4},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
 	};
 }
 
 cpuLR35902::~cpuLR35902()
 {
+}
+
+uint8_t cpuLR35902::GetFlag(FLAGS35902 f)
+{
+	return ((status & f) > 0) ? 1 : 0;
+}
+
+void cpuLR35902::SetFlag(FLAGS35902 f, bool v)
+{
+	if (v)
+		status |= f;
+	else
+		status &= ~f;
+}
+
+uint8_t cpuLR35902::fetch()
+{
+	// TODO: If there are different types of addressing modes,
+	//       take care of them here
+	fetched = read(addr_abs);
+
+	return fetched;
+}
+
+uint8_t cpuLR35902::NOP()
+{
+	// TODO: Game compatibility may necessitate handling different
+	// NOP opcodes differently, thus the switch statement
+	switch (opcode)
+	{
+	case 0xFC:
+		return 1;
+		break;
+	}
+	return 0;
 }
--- cpuLR35902.h
+++ cpuLR35902.h
@@ -17,20 +17,23 @@ public:
 	// external event functions
 	// connect to bus
 	// CPU flags
-	// CPU instructions (counted 73)
+	// CPU instructions
 
 	// ===== REGISTERS =====
 	// Accumulator and flags
-	uint16_t af   = 0x0000; 
+	uint16_t af     = 0x0000; 
 	// The below 3 registers are general purpose 16 bit registers, 
 	// or they can be used as 2 separate 8 bit registers
-	uint16_t bc   = 0x0000; // High: B / LOW: C 
-	uint16_t de   = 0x0000; // High: D / LOW: E
-	uint16_t hl   = 0x0000; // High: H / LOW: L
+	uint16_t bc     = 0x0000; // High: B / LOW: C 
+	uint16_t de     = 0x0000; // High: D / LOW: E
+	uint16_t hl     = 0x0000; // High: H / LOW: L
 	// Procedural registers
-	uint16_t stkp = 0x0000; // stack pointer
-	uint16_t pc   = 0x0000; // program counter
+	uint16_t stkp   = 0x0000; // stack pointer
+	uint16_t pc     = 0x0000; // program counter
+	uint8_t  status = 0x00; // status register
+	
 
+	void reset();
 	void irq();
 	void nmi();
 	void clock();
@@ -41,13 +44,25 @@ public:
 	// upper nibble is the flags, lower nibble is always 0x0
 	enum FLAGS35902
 	{
-		Z = (1 << 7), // Zero flag
-		N = (1 << 6), // Subtraction flag (BCD)
+		C = (1 << 4), // Carry flag
 		H = (1 << 5), // Half carry flag (lower 4 bit carry) (BCD)
-		C = (1 << 4)  // Carry flag
+		N = (1 << 6), // Subtraction flag (BCD)
+		Z = (1 << 7)  // Zero flag
 	};
 
 private:
+	uint8_t GetFlag(FLAGS35902 f);
+	void    SetFlag(FLAGS35902 f, bool v);
+
+	// Assistance variables
+	uint8_t fetched      = 0x00;
+	uint16_t temp        = 0x0000;
+	uint16_t addr_abs    = 0x0000;
+	uint16_t addr_rel    = 0x0000; // TODO: Need this?
+	uint8_t opcode       = 0x00;
+	uint8_t cycles       = 0x00;
+	uint32_t clock_count = 0x00000000;
+
 	Bus* bus = nullptr;
 	uint8_t read(uint16_t a);
 	void    write(uint16_t a, uint8_t d);
@@ -58,7 +73,8 @@ private:
 	{
 		std::string name;
 		uint8_t (cpuLR35902::* operate)(void) = nullptr;
-		uint8_t (cpuLR35902::* addrmode)(void) = nullptr;
+		// it doesn't seem like GB has multiple addressing modes? keeping for now just in case
+		//uint8_t (cpuLR35902::* addrmode)(void) = nullptr; 
 		uint8_t cycles = 0;
 	};
 
@@ -66,56 +82,23 @@ private:
 
 private:
 	// ===== CPU INSTRUCTIONS =====
-	uint8_t NOP();
-	uint8_t LD();
-	uint8_t INC();
-	uint8_t DEC();
-	uint8_t ADD();
-	uint8_t RLCA();
-	uint8_t RRCA();
-	uint8_t RLA();
-	uint8_t RRA();
-	uint8_t DAA();
-	uint8_t CPL();
-	uint8_t SCF();
-	uint8_t CCF();
-	uint8_t JR();
-	uint8_t JR_COND();
-	uint8_t STOP();
-	uint8_t HALT();
-	uint8_t ADDC();
-	uint8_t SUB();
-	uint8_t SBC();
-	uint8_t AND();
-	uint8_t XOR();
-	uint8_t OR();
-	uint8_t CP();
-	uint8_t RET_COND();
-	uint8_t RET();
-	uint8_t RETi();
-	uint8_t JP_COND();
-	uint8_t JP();
-	uint8_t CALL_COND();
-	uint8_t CALL();
-	uint8_t RST();
-	uint8_t POP();
-	uint8_t PUSH();
-	uint8_t LDH();
-	uint8_t DI();
-	uint8_t EI();
-	// $CB
-	uint8_t RLC();
-	uint8_t RRC();
-	uint8_t RL();
-	uint8_t RR();
-	uint8_t SLA();
-	uint8_t SRA();
-	uint8_t SWAP();
-	uint8_t SRL();
-	uint8_t BIT();
-	uint8_t RES();
-	uint8_t SET();
-	// Catch all
-	uint8_t XXX();
+	uint8_t ADC();      uint8_t HALT();		uint8_t RETI();
+	uint8_t ADD();	 	uint8_t INC();		uint8_t RLA();
+	uint8_t AND();	 	uint8_t JP();		uint8_t RLCA();
+	uint8_t CALL();	 	uint8_t JR();		uint8_t RRA();
+	uint8_t CCF();	 	uint8_t LD();		uint8_t RRCA();
+	uint8_t CP();	 	uint8_t LDH();		uint8_t RST();
+	uint8_t CPL();	 	uint8_t NOP();		uint8_t SBC();
+	uint8_t DAA();	 	uint8_t OR();		uint8_t SCF();
+	uint8_t DEC();	 	uint8_t POP();		uint8_t STOP();
+	uint8_t DI();	 	uint8_t PUSH();		uint8_t SUB();
+	uint8_t EI();	 	uint8_t RET();		uint8_t XOR();
+
+	// IDK about these 3:
+	uint8_t JR_r8(); // ????????????????
+	uint8_t OR_d8(); // ????????????????
+	uint8_t PREFIX_CB(); // ??? This one is definitely important, how to handle this?
+	
+	uint8_t XXX(); // catch all for "???" instructions
 
 };
