773769d307a878e1b7c1e5103eb4621422e6c076
Logan Scholz <logans@ping.com>
2025-09-04 10:12:09 -0700

    populating lookup table

--- cpuLR35902.cpp
+++ cpuLR35902.cpp
@@ -4,32 +4,26 @@
 cpuLR35902::cpuLR35902()
 {
 	using a = cpuLR35902;
-	// each {} is an INSTRUCTION struct: 
-	// string name, pointer to opcode function, total cycles for instruction
-	// TODO 1: done
-	// TODO 2: There are a lot of instructions that operate on different registers (LD for example), 
-	//         will one LD function satisfy all the permutations? How to do this?
-	// TODO 3: Every opcode also has "length in bytes" that is not captured here... should it be? 
-	//         ...maybe this "length in bytes" attribute can be used to organize some kind of polymorphism?
 	lookup =
 	{
+		// TODO: Some cycles are in the form "12/8" or similar... figure this out. Probably handle this in the op code's function
 		// only doing 1 row for now to figure things out
-		{"NOP", &a::NOP, 4},{"LD", &a::LD, 12},{"LD", &a::LD, 8},{"INC", &a::INC, 8},{"INC", &a::INC, 4},{"DEC", &a::DEC, 4},{"LD", &a::LD, 8},{"RLCA", &a::RLCA, 4},{"LD", &a::LD, 20},{"ADD", &a::ADD, 8},{"LD", &a::LD, 8},{"DEC", &a::DEC, 8},{"INC", &a::INC, 4},{"DEC", &a::DEC, 4},{"LD", &a::LD, 8},{"RRCA", &a::RRCA, 4},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-		{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
+		{"NOP", &a::OP_00, 4},{"LD BC,d16", &a::OP_01, 12},{"LD (BC),A", &a::OP_02, 8},{"INC BC", &a::OP_03, 8},{"INC B", &a::OP_04, 4},{"DEC B", &a::OP_05, 4},{"LD B,d8", &a::OP_06, 8},{"RLCA", &a::OP_07, 4},{"LD (a16), SP", &a::OP_08, 20},{"ADD HL,BC", &a::OP_09, 8},{"LD A,(BC)", &a::OP_0A, 8},{"DEC BC", &a::OP_0B, 8},{"INC C", &a::OP_0C, 4},{"DEC C", &a::OP_0D, 4},{"LD C,d8", &a::OP_0E, 8},{"RRCA", &a::OP_0F, 4},
+		{"STOP", &a::OP_10, 4},{"LD DE,d16", &a::OP_11, 12},{"LD (DE),A", &a::OP_12, 8},{"INC DE", &a::OP_13, 8},{"INC D", &a::OP_14, 4},{"DEC D", &a::OP_15, 4},{"LD D,d8", &a::OP_16, 8},{"RLA", &a::OP_17, 4},{"JR r8", &a::OP_18, 12},{"ADD HL,DE", &a::OP_19, 8},{"LD A,(DE)", &a::OP_1A, 8},{"DEC DE", &a::OP_1B, 8},{"INC E", &a::OP_1C, 4},{"DEC E", &a::OP_1D, 4},{"LD E,d8", &a::OP_1E, 8},{"RRA", &a::OP_1F, 4},
+		{"JR NZ,r8", &a::OP_20, 12/8},{"LD HL,d16", &a::OP_21, 12},{"LD (HL+),A", &a::OP_22, 8},{"INC HL", &a::OP_23, 8},{"INC H", &a::OP_24, 4},{"DEC H", &a::OP_25, 4},{"LD H,d8", &a::OP_26, 8},{"DAA", &a::OP_27, 4},{"JR Z,r8", &a::OP_28, 12/8},{"ADD HL,HL", &a::OP_29, 8},{"LD A,(HL+)", &a::OP_2A, 8},{"DEC HL", &a::OP_2B, 8},{"INC L", &a::OP_2C, 4},{"DEC L", &a::OP_2D, 4},{"LD L,d8", &a::OP_2E, 8},{"CPL", &a::OP_2F, 4},
+		{"JR NC,r8", &a::OP_30, 12/8},{"LD SP,d16", &a::OP_31, 16},{"LD (HL-),A", &a::OP_32, 8},{"INC SP", &a::OP_33, 8},{"INC (HL)", &a::OP_34, 12},{"DEC (HL)", &a::OP_35, 12},{"LD (HL),d8", &a::OP_36, 12},{"SCF", &a::OP_37, 4},{"JR C,r8", &a::OP_38, 12/8},{"ADD HL,SP", &a::OP_39, 8},{"LD A,(HL-)", &a::OP_3A, 8},{"DEC SP", &a::OP_3B, 8},{"INC A", &a::OP_3C, 4},{"DEC A", &a::OP_3D, 4},{"LD A,d8", &a::OP_3E, 8},{"CCF", &a::OP_3F, 4},
+		{"LD B,B", &a::OP_40, 4},{"LD B,C", &a::OP_41, 4},{"LD B,D", &a::OP_42, 4},{"LD B,E", &a::OP_43, 4},{"LD B,H", &a::OP_44, 4},{"LD B,L", &a::OP_45, 4},{"LD B,(HL)", &a::OP_46, 8},{"LD B,A", &a::OP_47, 4},{"LD C,B", &a::OP_48, 4},{"LD C,C", &a::OP_49, 4},{"LD C,D", &a::OP_4A, 4},{"LD C,E", &a::OP_4B, 4},{"LD C,H", &a::OP_4C, 4},{"LD C,L", &a::OP_4D, 4},{"LD C,(HL)", &a::OP_4E, 8},{"LD C,A", &a::OP_4F, 4},
+		{"LD D,B", &a::OP_50, 4},{"LD D,C", &a::OP_51, 4},{"LD D,D", &a::OP_52, 4},{"LD D,E", &a::OP_53, 4},{"LD D,H", &a::OP_54, 4},{"LD D,L", &a::OP_55, 4},{"LD D,(HL)", &a::OP_56, 8},{"LD D,A", &a::OP_57, 4},{"LD E,B", &a::OP_58, 4},{"LD E,C", &a::OP_59, 4},{"LD E,D", &a::OP_5A, 4},{"LD E,E", &a::OP_5B, 4},{"LD E,H", &a::OP_5C, 4},{"LD E,L", &a::OP_5D, 4},{"LD E,(HL)", &a::OP_5E, 4},{"LD E,A", &a::OP_5F, 4},
+		{"LD H,B", &a::OP_60, 4},{"LD H,C", &a::OP_61, 4},{"LD H,D", &a::OP_62, 4},{"LD H,E", &a::OP_63, 4},{"LD H,H", &a::OP_64, 4},{"LD H,L", &a::OP_65, 4},{"LD H,(HL)", &a::OP_66, 8},{"LD H,A", &a::OP_67, 4},{"LD L,B", &a::OP_68, 4},{"LD L,C", &a::OP_69, 4},{"LD L,D", &a::OP_6A, 4},{"LD L,E", &a::OP_6B, 4},{"LD L,H", &a::OP_6C, 4},{"LD L,L", &a::OP_6D, 4},{"LD L,(HL)", &a::OP_6E, 8},{"LD L,A", &a::OP_6F, 4},
+		{"LD (HL),B", &a::OP_70, 8},{"LD (HL),C", &a::OP_71, 8},{"LD (HL),D", &a::OP_72, 8},{"LD (HL),E", &a::OP_73, 8},{"LD (HL),H", &a::OP_74, 8},{"LD (HL),L", &a::OP_75, 8},{"HALT", &a::OP_76, 4},{"LD (HL),A", &a::OP_77, 8},{"LD A,B", &a::OP_78, 4},{"LD A,C", &a::OP_79, 4},{"LD A,D", &a::OP_7A, 4},{"LD A,E", &a::OP_7B, 4},{"LD A,H", &a::OP_7C, 4},{"LD A,L", &a::OP_7D, 4},{"LD A,(HL)", &a::OP_7E, 8},{"LD A,A", &a::OP_7F, 4},
+		{"", &a::OP_80, ?},{"", &a::OP_81, ?},{"", &a::OP_82, ?},{"", &a::OP_83, ?},{"", &a::OP_84, ?},{"", &a::OP_85, ?},{"", &a::OP_86, ?},{"", &a::OP_87, ?},{"", &a::OP_88, ?},{"", &a::OP_89, ?},{"", &a::OP_8A, ?},{"", &a::OP_8B, ?},{"", &a::OP_8C, ?},{"", &a::OP_8D, ?},{"", &a::OP_8E, ?},{"", &a::OP_8F, ?},
+		{"", &a::OP_90, ?},{"", &a::OP_91, ?},{"", &a::OP_92, ?},{"", &a::OP_93, ?},{"", &a::OP_94, ?},{"", &a::OP_95, ?},{"", &a::OP_96, ?},{"", &a::OP_97, ?},{"", &a::OP_98, ?},{"", &a::OP_99, ?},{"", &a::OP_9A, ?},{"", &a::OP_9B, ?},{"", &a::OP_9C, ?},{"", &a::OP_9D, ?},{"", &a::OP_9E, ?},{"", &a::OP_9F, ?},
+		{"", &a::OP_A0, ?},{"", &a::OP_A1, ?},{"", &a::OP_A2, ?},{"", &a::OP_A3, ?},{"", &a::OP_A4, ?},{"", &a::OP_A5, ?},{"", &a::OP_A6, ?},{"", &a::OP_A7, ?},{"", &a::OP_A8, ?},{"", &a::OP_A9, ?},{"", &a::OP_AA, ?},{"", &a::OP_AB, ?},{"", &a::OP_AC, ?},{"", &a::OP_AD, ?},{"", &a::OP_AE, ?},{"", &a::OP_AF, ?},
+		{"", &a::OP_B0, ?},{"", &a::OP_B1, ?},{"", &a::OP_B2, ?},{"", &a::OP_B3, ?},{"", &a::OP_B4, ?},{"", &a::OP_B5, ?},{"", &a::OP_B6, ?},{"", &a::OP_B7, ?},{"", &a::OP_B8, ?},{"", &a::OP_B9, ?},{"", &a::OP_BA, ?},{"", &a::OP_BB, ?},{"", &a::OP_BC, ?},{"", &a::OP_BD, ?},{"", &a::OP_BE, ?},{"", &a::OP_BF, ?},
+		{"", &a::OP_C0, ?},{"", &a::OP_C1, ?},{"", &a::OP_C2, ?},{"", &a::OP_C3, ?},{"", &a::OP_C4, ?},{"", &a::OP_C5, ?},{"", &a::OP_C6, ?},{"", &a::OP_C7, ?},{"", &a::OP_C8, ?},{"", &a::OP_C9, ?},{"", &a::OP_CA, ?},{"", &a::OP_CB, ?},{"", &a::OP_CC, ?},{"", &a::OP_CD, ?},{"", &a::OP_CE, ?},{"", &a::OP_CF, ?},
+		{"", &a::OP_D0, ?},{"", &a::OP_D1, ?},{"", &a::OP_D2, ?},{"", &a::OP_D3, ?},{"", &a::OP_D4, ?},{"", &a::OP_D5, ?},{"", &a::OP_D6, ?},{"", &a::OP_D7, ?},{"", &a::OP_D8, ?},{"", &a::OP_D9, ?},{"", &a::OP_DA, ?},{"", &a::OP_DB, ?},{"", &a::OP_DC, ?},{"", &a::OP_DD, ?},{"", &a::OP_DE, ?},{"", &a::OP_DF, ?},
+		{"", &a::OP_E0, ?},{"", &a::OP_E1, ?},{"", &a::OP_E2, ?},{"", &a::OP_E3, ?},{"", &a::OP_E4, ?},{"", &a::OP_E5, ?},{"", &a::OP_E6, ?},{"", &a::OP_E7, ?},{"", &a::OP_E8, ?},{"", &a::OP_E9, ?},{"", &a::OP_EA, ?},{"", &a::OP_EB, ?},{"", &a::OP_EC, ?},{"", &a::OP_ED, ?},{"", &a::OP_EE, ?},{"", &a::OP_EF, ?},
+		{"", &a::OP_F0, ?},{"", &a::OP_F1, ?},{"", &a::OP_F2, ?},{"", &a::OP_F3, ?},{"", &a::OP_F4, ?},{"", &a::OP_F5, ?},{"", &a::OP_F6, ?},{"", &a::OP_F7, ?},{"", &a::OP_F8, ?},{"", &a::OP_F9, ?},{"", &a::OP_FA, ?},{"", &a::OP_FB, ?},{"", &a::OP_FC, ?},{"", &a::OP_FD, ?},{"", &a::OP_FE, ?},{"", &a::OP_FF, ?}
 	};
 }
 
@@ -58,7 +52,8 @@ uint8_t cpuLR35902::fetch()
 	return fetched;
 }
 
-uint8_t cpuLR35902::NOP()
+// NOP
+uint8_t cpuLR35902::OP_00()
 {
 	// TODO: Game compatibility may necessitate handling different
 	// NOP opcodes differently, thus the switch statement
--- cpuLR35902.h
+++ cpuLR35902.h
@@ -122,7 +122,7 @@ private:
 		uint8_t (cpuLR35902::* operate)(void) = nullptr;
 		
 		// it doesn't seem like GB has multiple addressing modes? keeping for now just in case
-		// UPDATE: THERE ARE DIFFERENT ADDRESSING MODES, AT LEAST 2 SO FAR:
+		// UPDATE: THERE ARE DIFFERENT ADDRESSING MODES, SO FAR:
 		// Immediate d8: Immediate 8 bit data
 		// Immediate d16: Immediate 16 bit data
 		// Implied: No additional data is required, just simple stuff like changing a status bit
@@ -142,24 +142,113 @@ private:
 	uint8_t ABS();
 
 private:
-	// ===== CPU INSTRUCTIONS =====
-	uint8_t ADC();      uint8_t HALT();		uint8_t RETI();
-	uint8_t ADD();	 	uint8_t INC();		uint8_t RLA();
-	uint8_t AND();	 	uint8_t JP();		uint8_t RLCA();
-	uint8_t CALL();	 	uint8_t JR();		uint8_t RRA();
-	uint8_t CCF();	 	uint8_t LD();		uint8_t RRCA();
-	uint8_t CP();	 	uint8_t LDH();		uint8_t RST();
-	uint8_t CPL();	 	uint8_t NOP();		uint8_t SBC();
-	uint8_t DAA();	 	uint8_t OR();		uint8_t SCF();
-	uint8_t DEC();	 	uint8_t POP();		uint8_t STOP();
-	uint8_t DI();	 	uint8_t PUSH();		uint8_t SUB();
-	uint8_t EI();	 	uint8_t RET();		uint8_t XOR();
-	uint8_t PREFIX_CB(); // ??? This one is definitely important, how to handle this?
-	uint8_t XXX(); // catch for all undefined opcodes
-
-	// IDK about these 2:
-	uint8_t JR_r8(); // ????????????????
-	uint8_t OR_d8(); // ????????????????
 	
+	// !!!!!!!!!!!!!!!!!!!!! OLDDDDDDDDD METHOD !!!!!!!!!!!!!!!!!!!!!!!!!
+	// ===== CPU INSTRUCTIONS =====
+	//uint8_t ADC();      uint8_t HALT();		uint8_t RETI();
+	//uint8_t ADD();	 	uint8_t INC();		uint8_t RLA();
+	//uint8_t AND();	 	uint8_t JP();		uint8_t RLCA();
+	//uint8_t CALL();	 	uint8_t JR();		uint8_t RRA();
+	//uint8_t CCF();	 	uint8_t LD();		uint8_t RRCA();
+	//uint8_t CP();	 	uint8_t LDH();		uint8_t RST();
+	//uint8_t CPL();	 	uint8_t NOP();		uint8_t SBC();
+	//uint8_t DAA();	 	uint8_t OR();		uint8_t SCF();
+	//uint8_t DEC();	 	uint8_t POP();		uint8_t STOP();
+	//uint8_t DI();	 	uint8_t PUSH();		uint8_t SUB();
+	//uint8_t EI();	 	uint8_t RET();		uint8_t XOR();
+	//uint8_t PREFIX_CB(); // ??? This one is definitely important, how to handle this?
+	//uint8_t XXX(); // catch for all undefined opcodes
+
+	//// IDK about these 2:
+	//uint8_t JR_r8(); // ????????????????
+	//uint8_t OR_d8(); // ????????????????
+	
+
+	//-o------------------------------------------------------------------o
+	// |   STANDARD OPCODES                                                     |
+	//-o------------------------------------------------------------------o
+	uint8_t OP_00(); uint8_t OP_10(); uint8_t OP_20(); uint8_t OP_30();	uint8_t OP_40(); uint8_t OP_50(); uint8_t OP_60(); uint8_t OP_70();
+	uint8_t OP_01(); uint8_t OP_11(); uint8_t OP_21(); uint8_t OP_31();	uint8_t OP_41(); uint8_t OP_51(); uint8_t OP_61(); uint8_t OP_71();
+	uint8_t OP_02(); uint8_t OP_12(); uint8_t OP_22(); uint8_t OP_32();	uint8_t OP_42(); uint8_t OP_52(); uint8_t OP_62(); uint8_t OP_72();
+	uint8_t OP_03(); uint8_t OP_13(); uint8_t OP_23(); uint8_t OP_33();	uint8_t OP_43(); uint8_t OP_53(); uint8_t OP_63(); uint8_t OP_73();
+	uint8_t OP_04(); uint8_t OP_14(); uint8_t OP_24(); uint8_t OP_34();	uint8_t OP_44(); uint8_t OP_54(); uint8_t OP_64(); uint8_t OP_74();
+	uint8_t OP_05(); uint8_t OP_15(); uint8_t OP_25(); uint8_t OP_35();	uint8_t OP_45(); uint8_t OP_55(); uint8_t OP_65(); uint8_t OP_75();
+	uint8_t OP_06(); uint8_t OP_16(); uint8_t OP_26(); uint8_t OP_36();	uint8_t OP_46(); uint8_t OP_56(); uint8_t OP_66(); uint8_t OP_76();
+	uint8_t OP_07(); uint8_t OP_17(); uint8_t OP_27(); uint8_t OP_37();	uint8_t OP_47(); uint8_t OP_57(); uint8_t OP_67(); uint8_t OP_77();
+	uint8_t OP_08(); uint8_t OP_18(); uint8_t OP_28(); uint8_t OP_38();	uint8_t OP_48(); uint8_t OP_58(); uint8_t OP_68(); uint8_t OP_78();
+	uint8_t OP_09(); uint8_t OP_19(); uint8_t OP_29(); uint8_t OP_39();	uint8_t OP_49(); uint8_t OP_59(); uint8_t OP_69(); uint8_t OP_79();
+	uint8_t OP_0A(); uint8_t OP_1A(); uint8_t OP_2A(); uint8_t OP_3A();	uint8_t OP_4A(); uint8_t OP_5A(); uint8_t OP_6A(); uint8_t OP_7A();
+	uint8_t OP_0B(); uint8_t OP_1B(); uint8_t OP_2B(); uint8_t OP_3B();	uint8_t OP_4B(); uint8_t OP_5B(); uint8_t OP_6B(); uint8_t OP_7B();
+	uint8_t OP_0C(); uint8_t OP_1C(); uint8_t OP_2C(); uint8_t OP_3C();	uint8_t OP_4C(); uint8_t OP_5C(); uint8_t OP_6C(); uint8_t OP_7C();
+	uint8_t OP_0D(); uint8_t OP_1D(); uint8_t OP_2D(); uint8_t OP_3D();	uint8_t OP_4D(); uint8_t OP_5D(); uint8_t OP_6D(); uint8_t OP_7D();
+	uint8_t OP_0E(); uint8_t OP_1E(); uint8_t OP_2E(); uint8_t OP_3E();	uint8_t OP_4E(); uint8_t OP_5E(); uint8_t OP_6E(); uint8_t OP_7E();
+	uint8_t OP_0F(); uint8_t OP_1F(); uint8_t OP_2F(); uint8_t OP_3F();	uint8_t OP_4F(); uint8_t OP_5F(); uint8_t OP_6F(); uint8_t OP_7F();
+					 
+					 
+	uint8_t OP_80(); uint8_t OP_90(); uint8_t OP_A0(); uint8_t OP_B0();	uint8_t OP_C0(); uint8_t OP_D0(); uint8_t OP_E0(); uint8_t OP_F0();
+	uint8_t OP_81(); uint8_t OP_91(); uint8_t OP_A1(); uint8_t OP_B1();	uint8_t OP_C1(); uint8_t OP_D1(); uint8_t OP_E1(); uint8_t OP_F1();
+	uint8_t OP_82(); uint8_t OP_92(); uint8_t OP_A2(); uint8_t OP_B2();	uint8_t OP_C2(); uint8_t OP_D2(); uint8_t OP_E2(); uint8_t OP_F2();
+	uint8_t OP_83(); uint8_t OP_93(); uint8_t OP_A3(); uint8_t OP_B3();	uint8_t OP_C3(); uint8_t OP_D3(); uint8_t OP_E3(); uint8_t OP_F3();
+	uint8_t OP_84(); uint8_t OP_94(); uint8_t OP_A4(); uint8_t OP_B4();	uint8_t OP_C4(); uint8_t OP_D4(); uint8_t OP_E4(); uint8_t OP_F4();
+	uint8_t OP_85(); uint8_t OP_95(); uint8_t OP_A5(); uint8_t OP_B5();	uint8_t OP_C5(); uint8_t OP_D5(); uint8_t OP_E5(); uint8_t OP_F5();
+	uint8_t OP_86(); uint8_t OP_96(); uint8_t OP_A6(); uint8_t OP_B6();	uint8_t OP_C6(); uint8_t OP_D6(); uint8_t OP_E6(); uint8_t OP_F6();
+	uint8_t OP_87(); uint8_t OP_97(); uint8_t OP_A7(); uint8_t OP_B7();	uint8_t OP_C7(); uint8_t OP_D7(); uint8_t OP_E7(); uint8_t OP_F7();
+	uint8_t OP_88(); uint8_t OP_98(); uint8_t OP_A8(); uint8_t OP_B8();	uint8_t OP_C8(); uint8_t OP_D8(); uint8_t OP_E8(); uint8_t OP_F8();
+	uint8_t OP_89(); uint8_t OP_99(); uint8_t OP_A9(); uint8_t OP_B9();	uint8_t OP_C9(); uint8_t OP_D9(); uint8_t OP_E9(); uint8_t OP_F9();
+	uint8_t OP_8A(); uint8_t OP_9A(); uint8_t OP_AA(); uint8_t OP_BA();	uint8_t OP_CA(); uint8_t OP_DA(); uint8_t OP_EA(); uint8_t OP_FA();
+	uint8_t OP_8B(); uint8_t OP_9B(); uint8_t OP_AB(); uint8_t OP_BB();	uint8_t OP_CB(); uint8_t OP_DB(); uint8_t OP_EB(); uint8_t OP_FB();
+	uint8_t OP_8C(); uint8_t OP_9C(); uint8_t OP_AC(); uint8_t OP_BC();	uint8_t OP_CC(); uint8_t OP_DC(); uint8_t OP_EC(); uint8_t OP_FC();
+	uint8_t OP_8D(); uint8_t OP_9D(); uint8_t OP_AD(); uint8_t OP_BD();	uint8_t OP_CD(); uint8_t OP_DD(); uint8_t OP_ED(); uint8_t OP_FD();
+	uint8_t OP_8E(); uint8_t OP_9E(); uint8_t OP_AE(); uint8_t OP_BE();	uint8_t OP_CE(); uint8_t OP_DE(); uint8_t OP_EE(); uint8_t OP_FE();
+	uint8_t OP_8F(); uint8_t OP_9F(); uint8_t OP_AF(); uint8_t OP_BF();	uint8_t OP_CF(); uint8_t OP_DF(); uint8_t OP_EF(); uint8_t OP_FF();
+
+
+	//-o------------------------------------------------------------------o
+	// |   CB OPCODES                                                     |
+	//-o------------------------------------------------------------------o
+	uint8_t OP_CB_00(); uint8_t OP_CB_10(); uint8_t OP_CB_20(); uint8_t OP_CB_30(); uint8_t OP_CB_40();	uint8_t OP_CB_50();	uint8_t OP_CB_60();	uint8_t OP_CB_70();
+	uint8_t OP_CB_01(); uint8_t OP_CB_11(); uint8_t OP_CB_21(); uint8_t OP_CB_31(); uint8_t OP_CB_41();	uint8_t OP_CB_51();	uint8_t OP_CB_61();	uint8_t OP_CB_71();
+	uint8_t OP_CB_02(); uint8_t OP_CB_12(); uint8_t OP_CB_22(); uint8_t OP_CB_32(); uint8_t OP_CB_42();	uint8_t OP_CB_52();	uint8_t OP_CB_62();	uint8_t OP_CB_72();
+	uint8_t OP_CB_03(); uint8_t OP_CB_13(); uint8_t OP_CB_23(); uint8_t OP_CB_33(); uint8_t OP_CB_43();	uint8_t OP_CB_53();	uint8_t OP_CB_63();	uint8_t OP_CB_73();
+	uint8_t OP_CB_04(); uint8_t OP_CB_14(); uint8_t OP_CB_24(); uint8_t OP_CB_34(); uint8_t OP_CB_44();	uint8_t OP_CB_54();	uint8_t OP_CB_64();	uint8_t OP_CB_74();
+	uint8_t OP_CB_05(); uint8_t OP_CB_15(); uint8_t OP_CB_25(); uint8_t OP_CB_35(); uint8_t OP_CB_45();	uint8_t OP_CB_55();	uint8_t OP_CB_65();	uint8_t OP_CB_75();
+	uint8_t OP_CB_06(); uint8_t OP_CB_16(); uint8_t OP_CB_26(); uint8_t OP_CB_36(); uint8_t OP_CB_46();	uint8_t OP_CB_56();	uint8_t OP_CB_66();	uint8_t OP_CB_76();
+	uint8_t OP_CB_07(); uint8_t OP_CB_17(); uint8_t OP_CB_27(); uint8_t OP_CB_37(); uint8_t OP_CB_47();	uint8_t OP_CB_57();	uint8_t OP_CB_67();	uint8_t OP_CB_77();
+	uint8_t OP_CB_08(); uint8_t OP_CB_18(); uint8_t OP_CB_28(); uint8_t OP_CB_38(); uint8_t OP_CB_48();	uint8_t OP_CB_58();	uint8_t OP_CB_68();	uint8_t OP_CB_78();
+	uint8_t OP_CB_09(); uint8_t OP_CB_19(); uint8_t OP_CB_29(); uint8_t OP_CB_39(); uint8_t OP_CB_49();	uint8_t OP_CB_59();	uint8_t OP_CB_69();	uint8_t OP_CB_79();
+	uint8_t OP_CB_0A(); uint8_t OP_CB_1A(); uint8_t OP_CB_2A(); uint8_t OP_CB_3A(); uint8_t OP_CB_4A();	uint8_t OP_CB_5A();	uint8_t OP_CB_6A();	uint8_t OP_CB_7A();
+	uint8_t OP_CB_0B(); uint8_t OP_CB_1B(); uint8_t OP_CB_2B(); uint8_t OP_CB_3B(); uint8_t OP_CB_4B();	uint8_t OP_CB_5B();	uint8_t OP_CB_6B();	uint8_t OP_CB_7B();
+	uint8_t OP_CB_0C(); uint8_t OP_CB_1C(); uint8_t OP_CB_2C(); uint8_t OP_CB_3C(); uint8_t OP_CB_4C();	uint8_t OP_CB_5C();	uint8_t OP_CB_6C();	uint8_t OP_CB_7C();
+	uint8_t OP_CB_0D(); uint8_t OP_CB_1D(); uint8_t OP_CB_2D(); uint8_t OP_CB_3D(); uint8_t OP_CB_4D();	uint8_t OP_CB_5D();	uint8_t OP_CB_6D();	uint8_t OP_CB_7D();
+	uint8_t OP_CB_0E(); uint8_t OP_CB_1E(); uint8_t OP_CB_2E(); uint8_t OP_CB_3E(); uint8_t OP_CB_4E();	uint8_t OP_CB_5E();	uint8_t OP_CB_6E();	uint8_t OP_CB_7E();
+	uint8_t OP_CB_0F(); uint8_t OP_CB_1F(); uint8_t OP_CB_2F(); uint8_t OP_CB_3F(); uint8_t OP_CB_4F();	uint8_t OP_CB_5F();	uint8_t OP_CB_6F();	uint8_t OP_CB_7F();
+
+
+	uint8_t OP_CB_80();	uint8_t OP_CB_90();	uint8_t OP_CB_A0();	uint8_t OP_CB_B0();	uint8_t OP_CB_C0();	uint8_t OP_CB_D0();	uint8_t OP_CB_E0();	uint8_t OP_CB_F0();
+	uint8_t OP_CB_81();	uint8_t OP_CB_91();	uint8_t OP_CB_A1();	uint8_t OP_CB_B1();	uint8_t OP_CB_C1();	uint8_t OP_CB_D1();	uint8_t OP_CB_E1();	uint8_t OP_CB_F1();
+	uint8_t OP_CB_82();	uint8_t OP_CB_92();	uint8_t OP_CB_A2();	uint8_t OP_CB_B2();	uint8_t OP_CB_C2();	uint8_t OP_CB_D2();	uint8_t OP_CB_E2();	uint8_t OP_CB_F2();
+	uint8_t OP_CB_83();	uint8_t OP_CB_93();	uint8_t OP_CB_A3();	uint8_t OP_CB_B3();	uint8_t OP_CB_C3();	uint8_t OP_CB_D3();	uint8_t OP_CB_E3();	uint8_t OP_CB_F3();
+	uint8_t OP_CB_84();	uint8_t OP_CB_94();	uint8_t OP_CB_A4();	uint8_t OP_CB_B4();	uint8_t OP_CB_C4();	uint8_t OP_CB_D4();	uint8_t OP_CB_E4();	uint8_t OP_CB_F4();
+	uint8_t OP_CB_85();	uint8_t OP_CB_95();	uint8_t OP_CB_A5();	uint8_t OP_CB_B5();	uint8_t OP_CB_C5();	uint8_t OP_CB_D5();	uint8_t OP_CB_E5();	uint8_t OP_CB_F5();
+	uint8_t OP_CB_86();	uint8_t OP_CB_96();	uint8_t OP_CB_A6();	uint8_t OP_CB_B6();	uint8_t OP_CB_C6();	uint8_t OP_CB_D6();	uint8_t OP_CB_E6();	uint8_t OP_CB_F6();
+	uint8_t OP_CB_87();	uint8_t OP_CB_97();	uint8_t OP_CB_A7();	uint8_t OP_CB_B7();	uint8_t OP_CB_C7();	uint8_t OP_CB_D7();	uint8_t OP_CB_E7();	uint8_t OP_CB_F7();
+	uint8_t OP_CB_88();	uint8_t OP_CB_98();	uint8_t OP_CB_A8();	uint8_t OP_CB_B8();	uint8_t OP_CB_C8();	uint8_t OP_CB_D8();	uint8_t OP_CB_E8();	uint8_t OP_CB_F8();
+	uint8_t OP_CB_89();	uint8_t OP_CB_99();	uint8_t OP_CB_A9();	uint8_t OP_CB_B9();	uint8_t OP_CB_C9();	uint8_t OP_CB_D9();	uint8_t OP_CB_E9();	uint8_t OP_CB_F9();
+	uint8_t OP_CB_8A();	uint8_t OP_CB_9A();	uint8_t OP_CB_AA();	uint8_t OP_CB_BA();	uint8_t OP_CB_CA();	uint8_t OP_CB_DA();	uint8_t OP_CB_EA();	uint8_t OP_CB_FA();
+	uint8_t OP_CB_8B();	uint8_t OP_CB_9B();	uint8_t OP_CB_AB();	uint8_t OP_CB_BB();	uint8_t OP_CB_CB();	uint8_t OP_CB_DB();	uint8_t OP_CB_EB();	uint8_t OP_CB_FB();
+	uint8_t OP_CB_8C();	uint8_t OP_CB_9C();	uint8_t OP_CB_AC();	uint8_t OP_CB_BC();	uint8_t OP_CB_CC();	uint8_t OP_CB_DC();	uint8_t OP_CB_EC();	uint8_t OP_CB_FC();
+	uint8_t OP_CB_8D();	uint8_t OP_CB_9D();	uint8_t OP_CB_AD();	uint8_t OP_CB_BD();	uint8_t OP_CB_CD();	uint8_t OP_CB_DD();	uint8_t OP_CB_ED();	uint8_t OP_CB_FD();
+	uint8_t OP_CB_8E();	uint8_t OP_CB_9E();	uint8_t OP_CB_AE();	uint8_t OP_CB_BE();	uint8_t OP_CB_CE();	uint8_t OP_CB_DE();	uint8_t OP_CB_EE();	uint8_t OP_CB_FE();
+	uint8_t OP_CB_8F();	uint8_t OP_CB_9F();	uint8_t OP_CB_AF();	uint8_t OP_CB_BF();	uint8_t OP_CB_CF();	uint8_t OP_CB_DF();	uint8_t OP_CB_EF();	uint8_t OP_CB_FF();
+
+
+
+
+
+
+
+
+
+
 
 };
