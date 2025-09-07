2df58c35eea24a8d93e47a8a26d3c868bfa98919
Brandon Grzenia <bpgrzenia@gmail.com>
2025-09-07 15:00:18 -0700

    fixing more issues

--- headers/Bus.h
+++ headers/Bus.h
@@ -2,20 +2,31 @@
 #include <cstdint>
 #include <array>
 
-// TODO: include CPU class
+#include "cpuLR35902.h"
+#include "ppu.h"
+#include "Cartridge.h"
 
 class Bus
 {
 public:
+
 	Bus();
 	~Bus();
 
 public:
-	// TODO: cpu object
 
-	std::array<uint8_t, 64 * 1024> ram; // TODO get GB ram size
+	cpuLR35902 cpu; 
+	ppu ppu;
+	Cartridge cart;
+	std::array<uint8_t, 8 * 1024> wRam; // 8kb work ram
+	std::array<uint8_t, 8 * 1024> vRam; // 8kb video ram
 
 public:
-	void write(uint16_t addr, uint8_t data);
-	uint8_t read(uint16_t addr, bool bReadOnly = false);
+
+	void writeWRAM(uint16_t addr, uint8_t data);
+	uint8_t readWRAM(uint16_t addr, bool bReadOnly = false);
+
+	void writeVRAM(uint16_t addr, uint8_t data);
+	uint8_t readVRAM(uint16_t addr, bool bReadOnly = false);
+
 };
