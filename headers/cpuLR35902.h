2916f2cacfe7e8345b7d2174b033988e81f2aa7f
Brandon <bpgrzenia@gmail.com>
2026-07-25 09:06:27 -0700

    made function header for opcode 0x03

--- headers/cpuLR35902.h
+++ headers/cpuLR35902.h
@@ -160,13 +160,14 @@ public:
 	uint8_t OP_00();
 	uint8_t OP_01();
 	uint8_t OP_02();
+	uint8_t OP_03();
 	
 	/*** OPCODES THAT ARE UNINITIALIZED WILL CAUSE COMPILER ISSUES ***
 	
 	uint8_t OP_10(); uint8_t OP_20(); uint8_t OP_30();	uint8_t OP_40(); uint8_t OP_50(); uint8_t OP_60(); uint8_t OP_70();
 	uint8_t OP_11(); uint8_t OP_21(); uint8_t OP_31();	uint8_t OP_41(); uint8_t OP_51(); uint8_t OP_61(); uint8_t OP_71();
 	uint8_t OP_02(); uint8_t OP_12(); uint8_t OP_22(); uint8_t OP_32();	uint8_t OP_42(); uint8_t OP_52(); uint8_t OP_62(); uint8_t OP_72();
-	uint8_t OP_03(); uint8_t OP_13(); uint8_t OP_23(); uint8_t OP_33();	uint8_t OP_43(); uint8_t OP_53(); uint8_t OP_63(); uint8_t OP_73();
+	uint8_t OP_13(); uint8_t OP_23(); uint8_t OP_33();	uint8_t OP_43(); uint8_t OP_53(); uint8_t OP_63(); uint8_t OP_73();
 	uint8_t OP_04(); uint8_t OP_14(); uint8_t OP_24(); uint8_t OP_34();	uint8_t OP_44(); uint8_t OP_54(); uint8_t OP_64(); uint8_t OP_74();
 	uint8_t OP_05(); uint8_t OP_15(); uint8_t OP_25(); uint8_t OP_35();	uint8_t OP_45(); uint8_t OP_55(); uint8_t OP_65(); uint8_t OP_75();
 	uint8_t OP_06(); uint8_t OP_16(); uint8_t OP_26(); uint8_t OP_36();	uint8_t OP_46(); uint8_t OP_56(); uint8_t OP_66(); uint8_t OP_76();
